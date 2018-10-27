668b3b6b9f093ce8b53139fc0e6cf59d9ead3007
kasunipavithra <wskasunipavithra@gmail.com>
2018-10-28 00:17:59 +0530

    Dashboard layout design finished

---
+++ src/app/ui/search-time/search-time.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-time',
+  templateUrl: './search-time.component.html',
+  styleUrls: ['./search-time.component.css']
+})
+export class SearchTimeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
